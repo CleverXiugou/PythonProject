6aaf91d813de577bde9041ae1cdf3aa81778d328
Eric_Mac <2017995291@qq.com>
2025-05-29 04:16:14 +0800

    mac测试上传1

--- PythonGO/05_字符串练习.py
+++ PythonGO/05_字符串练习.py
@@ -10,3 +10,15 @@ print(str2)
 # 3.将字符串以"|"为分割符,存入到列表中
 the_list=str2.split("|")
 print(the_list)
+
+# 股价计算小程序
+company_name="Wine"
+stock_price=15
+stock_code="021120110"
+stock_price_daily_growth_factor=1.2
+growth_day=7
+
+message=(f"公司名:{company_name},股票代码:{stock_code},当前股价:{stock_price}\n"
+         f"每日增长系数:{stock_price_daily_growth_factor},经过{growth_day}天的增长后,"
+         f"股价为:%.2f!" % (stock_price * stock_price_daily_growth_factor ** growth_day))
+print(message)
